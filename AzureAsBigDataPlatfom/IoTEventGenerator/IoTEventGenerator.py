c7a9d1e2f5699bc57a3322b90d97861b19540809
cloud4yourdata <cloud4yourdata@outlook.com>
2018-03-16 15:01:02 +0100

    Changes in Azure As Big Data Platform Demo

--- AzureAsBigDataPlatfom/IoTEventGenerator/IoTEventGenerator.py
+++ AzureAsBigDataPlatfom/IoTEventGenerator/IoTEventGenerator.py
@@ -1,18 +1,21 @@
 import devices as dv
 import time
+import os
 from azure.servicebus import ServiceBusService
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 serviceNamespace  = "bdpmydevlabs"
 sharedAccessKeyName = "RootManageSharedAccessKey"
 sharedAccessKeyValue = "t1oWq1KYT8eQJoSkqVQaPFBsdgtlTK7MtfU+A/FLSYk="
 eventHubName = "mydemodevices"
-
-
 devices = []
 for x in range(1, 11):
     devices.append(dv.DevMeasurement(x, x*10, 20, 50))
 
-sbs = ServiceBusService(service_namespace=serviceNamespace, shared_access_key_name=sharedAccessKeyName,
+sbs = ServiceBusService(service_namespace=serviceNamespace,
+                       shared_access_key_name=sharedAccessKeyName,
                        shared_access_key_value=sharedAccessKeyValue)
 while True:
   for dev in devices:
@@ -20,4 +23,6 @@ while True:
        eventData = dev.getJson()
        r = sbs.send_event(eventHubName, eventData)
        print(eventData)
-  time.sleep(5)
+       
+  time.sleep(2)
+  cls()
