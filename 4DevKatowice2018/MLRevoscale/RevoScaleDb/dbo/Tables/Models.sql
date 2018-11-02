e3dcab387785d955bd5aee4b0d0b1c387fa173d8
cloud4yourdata <cloud4yourdata@outlook.com>
2018-11-02 19:22:51 +0100

    Added 4Dev Katowice presentations

--- 4DevKatowice2018/MLRevoscale/RevoScaleDb/dbo/Tables/Models.sql
+++ 4DevKatowice2018/MLRevoscale/RevoScaleDb/dbo/Tables/Models.sql
@@ -3,6 +3,5 @@
 	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	[ModelName] [VARCHAR](30) NOT NULL,
 	[ModelLanguage] [VARCHAR](20) NOT NULL,
-	[Model] [varbinary](max) NOT NULL,
-	[RMSE] FLOAT
+	[Model] [VARBINARY](MAX) NOT NULL
 )
